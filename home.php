17bf0e844ea3d05834f3fc671f1498c969712885
RyanAlt222 <RyanSantos333@proton.me>
2025-07-02 11:42:25 -0600

    Add starting code

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scammerica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'includes/html_files/header.html'; ?>
+
+    <div id="title-container">
+        <h1 id="page-title">Scammerica</h1>
+    </div>
+    
+    <div id="description-container">
+        <div id="description-image"><img src="hands_chained.png" alt=""></div>
+        <div id="page-description" ><p>Everything you have been taught is a lie.
+            From the school system to work, your whole life was built on one goal in mind.
+            How much money, energy, and time can we extract from this individual. That's 
+            the American Dream
+        </s>
+        </p></div>
+        
+    </div>
+
+  <section id="blog-container">
+        <h2>Must Read</h2>
+        <div id="card">
+            <img src="gray.jpg" alt="">
+            <p>some random description</p>
+            <button>Read</button>
+        </div>
+        
+  </section>
+</body>
+</html>
